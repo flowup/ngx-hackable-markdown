a8a03358e27fce9326ab2b9ef0dcbcb81a42cdcc
Pavel Tobias <pavel.tobias@flowup.cz>
2020-07-16 16:31:24 +0200

    fix(lib): change detection forced; inputted markdown sanitized

--- projects/ngx-hackable-markdown/src/components/root.component.ts
+++ projects/ngx-hackable-markdown/src/components/root.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnDestroy, ViewChild, ViewContainerRef, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, ViewChild, ViewContainerRef, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { PseudoTagName, TemplatableTagName, AstNode } from '../utilities/types';
 import { parseMarkdown } from '../utilities/parser';
 import { TemplateService } from '../services/template.service';
-import { delay } from 'rxjs/operators';
 
 @Component({
   selector: '[ngxHackableMarkdown]',
   templateUrl: './root.component.html',
   providers: [TemplateService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RootComponent implements OnInit, OnDestroy {
   readonly TemplatableTagName = TemplatableTagName;
@@ -21,18 +21,21 @@ export class RootComponent implements OnInit, OnDestroy {
   }
 
   @Input('ngxHackableMarkdown')
-  set markdown(markdown: string) {
-    this.astRoot$.next(parseMarkdown(markdown));
+  set markdown(markdown: unknown) {
+    const rootNode = parseMarkdown(Boolean(markdown) ? String(markdown) : '');
+    this.astRoot$.next(rootNode);
   }
 
   private readonly astRoot$ = new BehaviorSubject<AstNode | null>(null);
   private readonly viewContainer$ = new BehaviorSubject<ViewContainerRef | null>(null);
 
-  constructor(private readonly templates: TemplateService) { }
+  constructor(
+    private readonly templates: TemplateService,
+    private readonly cdr: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
     combineLatest(this.astRoot$, this.viewContainer$, this.templates.ready$)
-      .pipe(delay(0)) // ExpressionChangedAfterItHasBeenCheckedError
       .subscribe(([astRoot, viewContainer]) => {
         if (astRoot == null || viewContainer == null) {
           return;
@@ -43,6 +46,8 @@ export class RootComponent implements OnInit, OnDestroy {
           this.templates.get(astRoot.tagName),
           astRoot
         );
+
+        this.cdr.detectChanges();
       });
   }
 
